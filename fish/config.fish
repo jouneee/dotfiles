5c4f2db75bd51f66a0202d428de5e4b6f50bf059
joune <jounerc@yandex.ru>
2026-02-18 07:55:07 +0300

    update

---
+++ fish/config.fish
@@ -0,0 +1,40 @@
+if status is-login
+    if uwsm check may-start && uwsm select
+        exec uwsm start default
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr -a nv nvim
+
+abbr -a f flatpak
+abbr -a fu flatpak update
+abbr -a fi flatpak install
+abbr -a fiu flatpak install --user
+abbr -a s sudo pacman -S 
+abbr -a sy sudo pacman -Sy
+abbr -a ss pacman -Ss 
+abbr -a syu sudo pacman -Syu
+abbr -a yss yay -Ss
+
+abbr -a sc sudo systemctl 
+abbr -a scu systemctl --user
+
+abbr -a +x chmod +x
+abbr -a +X chmod +x
+abbr -a ll "ls -lh" 
+abbr -a la "ls -lah" 
+abbr -a 2. cd ../.. 
+abbr -a 3. cd ../../.. 
+
+function fish_prompt
+    set_color green
+    printf ' %s' (prompt_pwd)
+    set_color cyan
+    printf ' > '
+    set_color normal
+    printf ''
+end
